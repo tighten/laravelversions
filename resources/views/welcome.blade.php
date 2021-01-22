94bfdd06c520d1c426a8d31d78e79633de209245
Matt Stauffer <matt@tighten.co>
2021-01-21 21:59:03 -0500

    Drop the date off estimated future releases

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,53 +29,53 @@
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body class="antialiased font-sans bg-gray-100 pt-12">
-        <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-            <h1 class="block text-5xl text-bold mb-4">Laravel Versions</h1>
+    <body class="pt-12 font-sans antialiased bg-gray-100">
+        <div class="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8">
+            <h1 class="block mb-4 text-5xl text-bold">Laravel Versions</h1>
             <p class="max-w-2xl mb-8 text-lg">Release dates and security/bugfix timelines for all versions of Laravel.</p>
 
-            <div class="mb-8 max-w-xs">
+            <div class="max-w-xs mb-8">
                 <div class="float-right cursor-pointer hover:text-blue-800" id="colorblind-mode-toggle">Colorblind mode</div>
                 <div class="font-bold">Colors:</div>
 
                 <div class="p-2 bg-red-300">
-                    <div class="float-right font-bold js-colorblind font-bold hidden">EOL</div>
+                    <div class="hidden float-right font-bold js-colorblind">EOL</div>
                     End of Life
                 </div>
                 <div class="p-2 bg-yellow-300">
-                    <div class="float-right font-bold js-colorblind font-bold hidden">SEC</div>
+                    <div class="hidden float-right font-bold js-colorblind">SEC</div>
                     Security fixes only
                 </div>
                 <div class="p-2 bg-green-300">
-                    <div class="float-right font-bold js-colorblind font-bold hidden">ALL</div>
+                    <div class="hidden float-right font-bold js-colorblind">ALL</div>
                     Bug and security fixes
                 </div>
             </div>
 
             <p class="max-w-3xl mb-8">To learn more about Laravel's versioning strategy, check out the <a href="https://laravel-news.com/laravel-releases" class="text-blue-800 underline hover:text-blue-600">Laravel news "Laravel Releases" page</a>.</p>
 
-            <h2 class="block text-xl font-bold mb-2">Currently supported versions</h2>
+            <h2 class="block mb-2 text-xl font-bold">Currently supported versions</h2>
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg mb-8">
+                    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <div class="mb-8 overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
                             <table class="min-w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
                                 <tr>
                                     <th scope="col"></th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                         Version
                                     </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                         Release date
                                     </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                         Bug Fixes Until
                                     </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                         Security Fixes Until
                                     </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                         LTS?
                                     </th>
                                 </tr>
@@ -96,22 +96,22 @@
                             @foreach ($activeVersions as $version)
                                 <tr>
                                     <th scope="col" class="{{ $statusClassMap[$version->status] }}">
-                                        <span class="js-colorblind hidden">{{ $statusTextMap[$version->status] }}</span>
+                                        <span class="hidden js-colorblind">{{ $statusTextMap[$version->status] }}</span>
                                         &nbsp;
                                     </th>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
                                         {{ $version->major }} {{ $version->released_at->gt(now()) ? '(not released yet!)' : '' }}
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        {{ $version->released_at->format('F j, Y') }} {{ $version->released_at->gt(now()) ? '(estimated)' : '' }}
+                                    <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                        {{ $version->released_at->gt(now()) ? $version->released_at->format('F, Y') . ' (estimated)' : $version->released_at->format('F j, Y') }}
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                         {{ $version->ends_bugfixes_at ? $version->ends_bugfixes_at->format('F j, Y'): '' }}
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                         {{ $version->ends_securityfixes_at ? $version->ends_securityfixes_at->format('F j, Y') : '' }}
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                         {{ $version->is_lts ? '✓' : '' }}
                                     </td>
                                 </tr>
@@ -123,28 +123,28 @@
                 </div>
             </div>
 
-            <h2 class="block text-xl font-bold mb-2">No longer receiving security updates!</h2>
+            <h2 class="block mb-2 text-xl font-bold">No longer receiving security updates!</h2>
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg mb-8">
+                    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <div class="mb-8 overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
                             <table class="min-w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
                                 <tr>
                                     <th scope="col"></th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                         Version
                                     </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                         Release date
                                     </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                         Bug Fixes Until
                                     </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                         Security Fixes Until
                                     </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                         LTS?
                                     </th>
                                 </tr>
@@ -153,22 +153,22 @@
                             @foreach ($inactiveVersions as $version)
                                 <tr>
                                     <th scope="col" class="{{ $statusClassMap[$version->status] }}">
-                                        <span class="js-colorblind hidden">{{ $statusTextMap[$version->status] }}</span>
+                                        <span class="hidden js-colorblind">{{ $statusTextMap[$version->status] }}</span>
                                         &nbsp;
                                     </th>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
                                         {{ $version->major }}{{ $version->major < 6 ? '.' . $version->minor : '' }}
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                         {{ $version->released_at->format('F j, Y') }} {{ $version->released_at->gt(now()) ? '(estimated)' : '' }}
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                         {{ $version->ends_bugfixes_at ? $version->ends_bugfixes_at->format('F j, Y'): '' }}
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                         {{ $version->ends_securityfixes_at ? $version->ends_securityfixes_at->format('F j, Y') : '' }}
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                         {{ $version->is_lts ? '✓' : '' }}
                                     </td>
                                 </tr>
@@ -179,10 +179,10 @@
                     </div>
                 </div>
             </div>
-            <div class="text-center my-8">
-                Brought to you by the lovely folks at <a href="https://tighten.co/" class="text-blue-800 hover:text-blue-600 underline">Tighten</a>.
+            <div class="my-8 text-center">
+                Brought to you by the lovely folks at <a href="https://tighten.co/" class="text-blue-800 underline hover:text-blue-600">Tighten</a>.
                 <br>
-                <a href="https://github.com/tighten/laravelversions" class="text-blue-800 hover:text-blue-600 underline">Source on GitHub</a> | <a href="/api/versions/laravel" class="text-blue-800 hover:text-blue-600 underline">Data available in JSON format</a>
+                <a href="https://github.com/tighten/laravelversions" class="text-blue-800 underline hover:text-blue-600">Source on GitHub</a> | <a href="/api/versions/laravel" class="text-blue-800 underline hover:text-blue-600">Data available in JSON format</a>
             </div>
         </div>
 
