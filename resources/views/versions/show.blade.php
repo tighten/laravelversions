e2f00eedefd968a320069ce2a5fca5850c39ee9d
Matt Stauffer <matt@tighten.co>
2021-01-22 11:00:14 -0500

    Add a bit more data to the still rudimentary individual version page

--- resources/views/versions/show.blade.php
+++ resources/views/versions/show.blade.php
@@ -1,37 +1,30 @@
 <x-app-layout>
     <h1 class="block mb-4 text-5xl text-bold">Laravel Version: <span>{{ $path }}</span></h1>
 
-    <div class="max-w-xs mb-8">
-        <div class="float-right cursor-pointer hover:text-blue-800" id="colorblind-mode-toggle">Colorblind mode</div>
-        <div class="font-bold">Colors:</div>
-
-        <div class="p-2 bg-red-300">
-            <div class="hidden float-right font-bold js-colorblind">EOL</div>
-            End of Life
-        </div>
-        <div class="p-2 bg-yellow-300">
-            <div class="hidden float-right font-bold js-colorblind">SEC</div>
-            Security fixes only
-        </div>
-        <div class="p-2 bg-green-300">
-            <div class="hidden float-right font-bold js-colorblind">ALL</div>
-            Bug and security fixes
-        </div>
-    </div>
-
-    <p class="max-w-3xl mb-8">To learn more about Laravel's versioning strategy, check out the <a href="https://laravel-news.com/laravel-releases" class="text-blue-800 underline hover:text-blue-600">Laravel news "Laravel Releases" page</a>.</p>
-
-    <h2 class="block mb-2 text-xl font-bold">Currently supported versions</h2>
     <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <h2 class="text-xl font-bold">Status:</h2>
+                <p class="mb-12 text-lg">
+                @switch($version->status)
+                    @case('active')
+                        Active Support
+                        @break
+                    @case('security')
+                        Security Fixes Only
+                        @break
+                    @case('inactive')
+                        No bug or security fixes! <strong>Upgrade as soon as possible.</strong>
+                        @break
+                @endswitch
+                </p>
                 <div class="mb-8 overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
                     <table class="min-w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
                         <tr>
                             <th scope="col"></th>
                             <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-                                Version
+                                Major Version
                             </th>
                             <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
                                 Release date
@@ -67,7 +60,7 @@
                                 &nbsp;
                             </th>
                             <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
-                                {{ $version->major }} {{ $version->released_at->gt(now()) ? '(not released yet!)' : '' }}
+                                {{ $version->major < 6 ? $version->major . '.' . $version->minor : $version->major }} {{ $version->released_at->gt(now()) ? '(not released yet!)' : '' }}
                             </td>
                             <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                 {{ $version->released_at->gt(now()) ? $version->released_at->format('F, Y') . ' (estimated)' : $version->released_at->format('F j, Y') }}
@@ -86,6 +79,18 @@
                     </tbody>
                     </table>
                 </div>
+
+                <br>
+            <!--
+
+                // todo:
+                - latest patch
+                - status
+                - dangerous?
+                - recommendation
+            -->
+
+            <p class="max-w-3xl mb-6">To learn more about Laravel's versioning strategy, check out the <a href="https://laravel-news.com/laravel-releases" class="text-blue-800 underline hover:text-blue-600">Laravel news "Laravel Releases" page</a>.</p>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,24 @@ Route::get('{version}', function ($path) {
     // @todo regex this with someone who's smarter at regex-ing... this is obviously super dumb
     $segments = explode('.', $path);
 
-    $version = LaravelVersion::where([
-        'major' => $segments[0],
-    ])->firstOrFail();
+    if ((int) $segments[0] === 0) {
+        abort(404);
+    }
+
+    if ($segments[0] < 6) {
+        if (! isset($segments[1])) {
+            abort(404);
+        }
+
+        $version = LaravelVersion::where([
+            'major' => $segments[0],
+            'minor' => $segments[1],
+        ])->firstOrFail();
+    } else {
+        $version = LaravelVersion::where([
+            'major' => $segments[0],
+        ])->firstOrFail();
+    }
 
     return view('versions.show', [
         'version' => $version,
