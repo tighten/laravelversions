e382d982843068cf52f655010c968064c66334cb
Olsza <olsza@users.noreply.github.com>
2021-02-12 19:55:38 +0100

    fix after test TLint

--- resources/views/includes/langs.blade.php
+++ resources/views/includes/langs.blade.php
@@ -3,8 +3,8 @@
 <ul class="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8 -mt-9 float-right">
     @foreach (Config::get('localized-routes.supported-locales', []) as $lang)
         <li class="inline-block">
-            <a hreflang="{{$lang}}" href="{{ \Route::localizedUrl($lang) }}" title="{{ $lang }}">
-            <span class="flag-icon flag-icon-{{$lang}}"></span>
+            <a hreflang="{{ $lang }}" href="{{ \Route::localizedUrl($lang) }}" title="{{ $lang }}">
+            <span class="flag-icon flag-icon-{{ $lang }}"></span>
             </a>
         </li>
     @endforeach
