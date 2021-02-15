93ef5d7d07b0bb7a07f0ef997ad30ec75d33200f
Olsza <olsza@users.noreply.github.com>
2021-02-15 23:42:42 +0100

    show list of language names

--- resources/views/includes/langs.blade.php
+++ resources/views/includes/langs.blade.php
@@ -1,13 +1,9 @@
 @if (Config::has('localized-routes.supported-locales'))
-
-<ul class="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8 -mt-9 float-right">
-    @foreach (Config::get('localized-routes.supported-locales', []) as $lang)
-        <li class="inline-block">
-            <a hreflang="{{ $lang }}" href="{{ Route::localizedUrl($lang) }}" title="{{ $lang }}">
-            <span class="flag-icon flag-icon-{{ $lang }}"></span>
-            </a>
-        </li>
-    @endforeach
-</ul>
-
+    <select name="forma" onchange="location = this.value;" class="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8 -mt-9 float-right lang">
+        @foreach (Config::get('localized-routes.supported-locales', []) as $lang)
+            <option value="{{ Route::localizedUrl($lang) }}" @if (App::getLocale() == $lang) selected="selected" @endif class="lang-name lang-name-{{ $lang }}">
+                {{ Config::get("localized-routes.locales-name-native.$lang", Str::upper($lang)) }}
+            </option>
+        @endforeach
+   </select>
 @endif
