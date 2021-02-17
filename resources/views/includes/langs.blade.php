8c31328f68df93783cfe1eb8f81dfb177114d962
Olsza <olsza@users.noreply.github.com>
2021-02-17 18:33:53 +0100

    fix menu style language

--- resources/views/includes/langs.blade.php
+++ resources/views/includes/langs.blade.php
@@ -1,5 +1,5 @@
 @if (Config::has('localized-routes.supported-locales'))
-    <select name="forma" onchange="location = this.value;" class="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8 -mt-9 float-right lang">
+    <select name="forma" onchange="location = this.value;" class="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8 -mt-9 float-right lang bg-gray-200 border border-gray-200 text-black text-xs rounded">
         @foreach (Config::get('localized-routes.supported-locales', []) as $lang)
             <option value="{{ Route::localizedUrl($lang) }}" @if (App::getLocale() == $lang) selected="selected" @endif class="lang-name lang-name-{{ $lang }}">
                 {{ Config::get("localized-routes.locales-name-native.$lang", Str::upper($lang)) }}
