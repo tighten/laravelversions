141c47affc2ebc7ffb3df276d741f81a2e3c0a91
Marcy Acevedo <marcy@tighten.co>
2021-02-22 11:02:12 -0600

    Update color palette.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
     colors: {
       'gray-50': '#FAFAFA',
       'gray-100': '#F3F4F6',
+      'gray-200': '#E4E4E7',
+      'gray-500':  '#71717A',
       'gray-700': '#2E3036',
       'red-300': '#DE8888',
       'yellow-300': '#F2D987',
