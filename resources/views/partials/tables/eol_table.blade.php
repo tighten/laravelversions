2f0ade3690f06207b66f46fce4c282b93cd85631
Olsza <olsza@users.noreply.github.com>
2021-03-03 13:00:58 +0100

    improved code formatting

--- resources/views/partials/tables/eol_table.blade.php
+++ resources/views/partials/tables/eol_table.blade.php
@@ -40,13 +40,25 @@
                                     <a href="{{ $version->url }}" class="border-hover">{{ $version->major }}{{ $version->major < 6 ? '.' . $version->minor : '' }}</a>
                                 </th>
                                 <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-                                    {{ $version->released_at->translatedFormat(__('DateLongFormat')) }} {{ $version->released_at->gt(now()) ? '(' . __('estimated') . ')' : '' }}
+                                    {{ 
+                                        $version->released_at->translatedFormat(__('DateLongFormat')) }} {{ $version->released_at->gt(now())
+                                            ? '(' . __('estimated') . ')'
+                                            : ''
+                                    }}
                                 </td>
                                 <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-                                    {{ $version->ends_bugfixes_at ? $version->ends_bugfixes_at->translatedFormat(__('DateLongFormat')): '' }}
+                                    {{ 
+                                        $version->ends_bugfixes_at
+                                            ? $version->ends_bugfixes_at->translatedFormat(__('DateLongFormat'))
+                                            : ''
+                                    }}
                                 </td>
                                 <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-                                    {{ $version->ends_securityfixes_at ? $version->ends_securityfixes_at->translatedFormat(__('DateLongFormat')) : '' }}
+                                    {{ 
+                                        $version->ends_securityfixes_at
+                                            ? $version->ends_securityfixes_at->translatedFormat(__('DateLongFormat'))
+                                            : ''
+                                    }}
                                 </td>
                                 <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                     {{ $version->is_lts ? '✓' : '' }}
