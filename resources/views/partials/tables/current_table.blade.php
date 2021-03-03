bee676dba131ee2104ffbe711c7e863077ba7f93
Olsza <olsza@users.noreply.github.com>
2021-03-03 12:39:26 +0100

    add indents

--- resources/views/partials/tables/current_table.blade.php
+++ resources/views/partials/tables/current_table.blade.php
@@ -33,23 +33,23 @@
                             <th class="px-6 py-4 text-sm font-medium text-left text-gray-900 whitespace-nowrap">
                                 <a href="{{ $version->url }}" class="border-hover">{{ $version->major }} {{
                                     $version->released_at->gt(now())
-                                    ? '(' . __('not released yet!') . ')'
-                                    : ''
+                                        ? '(' . __('not released yet!') . ')'
+                                        : ''
                                 }}</a>
                             </th>
                             <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                 {{ 
                                     $version->released_at->gt(now())
-                                    ? $version->released_at->translatedFormat(__('DateShortFormat')) . ' (' . __('estimated') . ')'
-                                    : $version->released_at->translatedFormat(__('DateLongFormat'))
+                                        ? $version->released_at->translatedFormat(__('DateShortFormat')) . ' (' . __('estimated') . ')'
+                                        : $version->released_at->translatedFormat(__('DateLongFormat'))
                                 }}
                             </td>
                             <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                 @if ($version->ends_bugfixes_at)
                                     {{ 
                                         $version->released_at->gt(now())
-                                        ? $version->ends_bugfixes_at->translatedFormat(__('DateShortFormat')) . ' (' . __('estimated') . ')'
-                                        : $version->ends_bugfixes_at->translatedFormat(__('DateLongFormat'))
+                                            ? $version->ends_bugfixes_at->translatedFormat(__('DateShortFormat')) . ' (' . __('estimated') . ')'
+                                            : $version->ends_bugfixes_at->translatedFormat(__('DateLongFormat'))
                                     }}
                                 @endif
                             </td>
@@ -57,8 +57,8 @@
                                 @if ($version->ends_securityfixes_at)
                                     {{ 
                                         $version->released_at->gt(now())
-                                        ? $version->ends_securityfixes_at->translatedFormat(__('DateShortFormat')) . ' (' . __('estimated') . ')'
-                                        : $version->ends_securityfixes_at->translatedFormat(__('DateLongFormat'))
+                                            ? $version->ends_securityfixes_at->translatedFormat(__('DateShortFormat')) . ' (' . __('estimated') . ')'
+                                            : $version->ends_securityfixes_at->translatedFormat(__('DateLongFormat'))
                                     }}
                                 @endif
                             </td>
