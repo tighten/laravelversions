4d4ee3a90240f6b01c0d5de8e51aa6d36e237560
Shift <shift@laravelshift.com>
2023-01-13 17:02:41 +0000

    Remove `fruitcake/laravel-cors` dependency

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \App\Http\Middleware\TrustProxies::class,
         // \App\Http\Middleware\TrustHosts::class,
-        \Fruitcake\Cors\HandleCors::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
