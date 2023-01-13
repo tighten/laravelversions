bacddbd75c6e9dea750123cc7a1e315df1a0d29f
Anthony Clark <anthonyclark@gmail.com>
2023-01-13 14:53:59 -0800

    Default lang direction to ltr and move to <html>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html dir="{{ __('dir') === 'rtl'? 'rtl' : 'ltr' }}" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -32,7 +32,7 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased bg-gray-100" dir="{{ __('dir') }}">
+    <body class="font-sans antialiased bg-gray-100">
         <div id="app">
             @include('partials/modules/header')
             <div class="max-w-screen-xl px-4 pt-12 mx-auto sm:px-6 lg:px-8">
