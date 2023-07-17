e04befb098a1a857e087a6673ef58c4712d7ae10
Anthony Clark <anthonyclark@gmail.com>
2023-07-17 13:13:29 -0500

    Dusting

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfAuthenticated
@@ -18,7 +17,7 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
+            if (auth()->guard($guard)->check()) {
                 return redirect(RouteServiceProvider::HOME);
             }
         }
