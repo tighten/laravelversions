5af0c0e72366961a2ef2e9cc5a0d25787f513b2f
Mateus Junges <mateus@junges.dev>
2024-08-09 18:48:22 -0300

    Remove final
    Co-authored-by: Jake Bathman <jake.bathman@gmail.com>

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
-final class RouteServiceProvider extends ServiceProvider
+class RouteServiceProvider extends ServiceProvider
 {
     /**
      * The path to the "home" route for your application.
